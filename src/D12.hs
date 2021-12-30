96097011ed070bf8956eea2ef2063e9eef856b77
Bhavit Sharma <bhavit.sharma804@gmail.com>
2021-12-30 05:57:18 +0530

    Add D12.hs.

--- src/D12.hs
+++ src/D12.hs
@@ -1,9 +1,47 @@
+import Data.Char (isLower)
+import Data.List (elemIndex)
+import Data.Map ((!))
 import qualified Data.Map as M
 
-type Graph = M.Map Int [Int]
+type Graph = M.Map String [String]
 
-solve :: Graph -> [Int] -> Int -> Int -> ()
-solve graph ones = 
-    where
+isSmall :: String -> Bool
+isSmall = all isLower
 
+-- for part1, replace condition with just `cnt == 0`
+canVisit :: String -> M.Map String Int -> Bool
+canVisit s m
+  | cnt == 0 = True
+  | cnt == 1 = 2 `notElem` M.elems m && s /= "start"
+  | otherwise = False
+  where
+    cnt = M.findWithDefault 0 s m
 
+-- constraints are small enough to just brute force instead of dp.
+-- Although implementing DP with haskell is so freakin hard.
+-- verified that answer can never be infinite because every "large" node is connected to one small.
+dfs :: String -> M.Map String Int -> Graph -> Int
+dfs st visited g
+  | st == "end" = 1
+  | not (canVisit st visited) = 0
+  | otherwise = foldl (\acc x -> acc + dfs x vis' g) 0 neighbors
+  where
+    neighbors = g ! st
+    vis' = if isSmall st then M.insertWith (+) st 1 visited else visited
+
+edge :: String -> (String, String)
+edge s = (x, drop 1 y)
+  where
+    (Just idx) = elemIndex '-' s
+    (x, y) = splitAt idx s
+
+formGraph :: [String] -> Graph
+formGraph lst = M.fromListWith (++) edges
+  where
+    e = fmap edge lst
+    edges = foldl (\acc (x, y) -> acc ++ [(x, [y]), (y, [x])]) [] e
+
+main :: IO ()
+main = do
+  contents <- readFile "src/input-12.txt"
+  print . dfs "start" M.empty . formGraph . lines $ contents
--- src/MonadTut.hs
+++ src/MonadTut.hs
@@ -1,5 +1,50 @@
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE TupleSections #-}
+
+import Control.Monad (ap, liftM) -- you will need to put this towards the top of the file
+
 data TurnstileState = Locked | Unlocked
   deriving (Eq, Show)
 
 data TurnstileOutput = Thank | Open | Tut
   deriving (Eq, Show)
+
+coin, push :: TurnstileState -> (TurnstileOutput, TurnstileState)
+coin _ = (Thank, Unlocked)
+push Locked = (Tut, Locked)
+push Unlocked = (Open, Locked)
+
+-- s === State, a === Output
+newtype State s a = State {runState :: s -> (a, s)}
+
+-- \s -> (b, s)
+-- a -> (s -> (b, s))
+
+instance Monad (State s) where
+  return :: a -> State s a
+  return x = State (x,) -- from TupleSections
+
+  (>>=) :: State s a -> (a -> State s b) -> State s b
+  p >>= k = State $ \s ->
+    let (a, s_1) = runState p s
+     in runState (k a) s_1
+
+instance Functor (State s) where
+  fmap = liftM
+
+instance Applicative (State s) where
+  pure = return
+  (<*>) = ap
+
+coinS, pushS :: State TurnstileState TurnstileOutput
+coinS = State coin
+pushS = State push
+
+mondayS :: State TurnstileState [TurnstileOutput]
+mondayS = do
+  a1 <- coinS
+  a2 <- pushS
+  a3 <- pushS
+  a4 <- coinS
+  a5 <- pushS
+  return [a1, a2, a3, a4, a5]
